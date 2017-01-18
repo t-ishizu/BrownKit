9f5c8d75b0a5c1a15ce3903a02d938ac16a3ada0
t-ishizu <t-ishizu@ist.osaka-u.ac.jp>
2017-01-18 17:41:20 +0900

    特に変化はない

--- performCCFinderX.py
+++ performCCFinderX.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # coding : UTF-8
 import sys
+import subprocess
 
 args = sys.argv
-print 'loading... ' + args[1]+'sha1.txt'
-hashFile=open(args[1]+"sha1.txt")
+print 'loading... ' + args[1]+'/../sha1.txt'
+hashFile=open(args[1]+"/../sha1.txt")
 line = hashFile.readline()
 
 while line:
-	print line
+	print line	
 	line = hashFile.readline()
 hashFile.close
