3887434201172b3b2df23c6279bd74621c4ac60c
takuya ishidu <takuya ishidu>
2017-01-19 12:53:31 +0900

    ハッシュ値が最新のリビジョンへのプロジェクトのチェックアウト機能実装

--- performCCFinderX.py
+++ performCCFinderX.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 # coding : UTF-8
 import sys
+import os
+import subprocess
 
 args = sys.argv
-print 'loading... ' + args[1]+'sha1.txt'
-hashFile=open(args[1]+"sha1.txt")
+print 'loading... ' + args[1]+'/../sha1.txt'
+hashFile=open(args[1]+"/../sha1.txt")
 line = hashFile.readline()
-
+os.chdir(args[1])
+cmd1 = "git checkout " + line
+ret1 = subprocess.check_call( cmd1 , shell=True)
+print ret1 == 0
 while line:
-	print line
+	#print line
 	line = hashFile.readline()
 hashFile.close
